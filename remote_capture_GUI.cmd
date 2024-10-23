1a3b0751605dee74b4260468c259e71ffab8b0ba
arjun-curibio <91759394+arjun-curibio@users.noreply.github.com>
2024-10-23 14:36:33 -0700

    create executable

---
+++ remote_capture_GUI.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'remote_capture_GUI.exe' with Python DLL being found.
+set PATH=C:\Users\achar\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\achar\AppData\Local\Programs\Python\Python311
+set NUITKA_PYTHONPATH=C:\Users\achar\OneDrive\Documents\GitHub\Zeus SWP GUI;C:\Users\achar\AppData\Local\Programs\Python\Python311\DLLs;C:\Users\achar\AppData\Local\Programs\Python\Python311\Lib;C:\Users\achar\AppData\Local\Programs\Python\Python311;C:\Users\achar\AppData\Roaming\Python\Python311\site-packages;C:\Users\achar\AppData\Roaming\Python\Python311\site-packages\win32;C:\Users\achar\AppData\Roaming\Python\Python311\site-packages\win32\lib;C:\Users\achar\AppData\Roaming\Python\Python311\site-packages\Pythonwin;C:\Users\achar\AppData\Local\Programs\Python\Python311\Lib\site-packages
+"%~dp0remote_capture_GUI.exe" %*
